4736a18779599b66ead8c6cbbb1ba6353f1c75aa
Emmanueel <64825533+Emmanueel@users.noreply.github.com>
2020-07-19 01:09:56 -0500

    Add files via upload

---
+++ Ejercicios_de_for.php
@@ -0,0 +1,29 @@
+<?php
+Print "<h2> Primer ejercico de FOR </h2> <br>";
+$a = 10;
+for ($a; $a>=1; $a--){
+    echo "$a<br>";
+}
+
+Print "<h2> Segundo ejercicio de FOR </h2> <br>";
+$a = 0;
+for ($a; $a<=1; $a++){
+    echo "$a<br>";
+}
+
+Print "<h2> Tercer ejercicio de FOR </h2> <br>";
+for ($i=1; $i <=10 ; $i=$i+1) {
+    echo $i. "<br> Aprobaron el curso ";
+}
+
+Print "<h2> Cuarto ejercicio de FOR </h2> <br>";
+for ($b=20; $b <=20 ; $b=$b+1) {
+    echo $b. "<br> Mi correo es al29374783@gmail.com";
+}
+
+Print "<h2> Quinto ejercicio de FOR </h2> <br>";
+$hola = 10;
+for ($hola; $hola>=10; $hola--) {
+    echo "$hola Hola <br>";
+}
+?>
