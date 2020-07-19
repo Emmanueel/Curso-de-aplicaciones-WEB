edf45eb97ee80a83136dff80371d7a28270833b8
Emmanueel <64825533+Emmanueel@users.noreply.github.com>
2020-07-19 01:12:31 -0500

    Add files via upload

---
+++ Ejercicios_de_if.php
@@ -0,0 +1,42 @@
+<?php
+Print "<h2> Primer ejemplo de IF </h2> <br>";
+$IRD32 = 50;
+if ($IRD32 >10) {
+    echo "Somos el Grupo IRD32";
+}else {
+    echo "No somos del Grupo IRD 32";
+}
+
+Print "<h2> Segundo ejemplo de IF </h2> <br>";
+$a = 12;
+if ($a >=10){
+    echo "Subes al autobus";
+}else {
+    echo "No subes al camion";
+}
+
+Print "<h2> Tercer ejemplo de IF </h2> <br>";
+$Galletas = 20;
+if ($Galletas =20){
+    echo "Si comes Galletas";
+}else {
+    echo "No comes Galletas";
+}
+
+Print "<h2> Cuarto ejemoplo de IF </h2> <br>";
+$Tortillas = 14;
+if ($Tortillas >=14){
+    echo "Si me alcanza para las torillas";
+}else {
+    echo "No me alcanza para las tortillas";
+}
+
+Print "<h2> Quinto Ejemplo de IF </h2> <br>";
+$a =10;
+$b =5;
+if ($a >=10 | $b =10){
+    echo "Soy rico";
+}else {
+    echo "No soy rico";
+}
+?>
