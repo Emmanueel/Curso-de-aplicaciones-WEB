031ea765cc73deae68404e0d93945302c71b87a0
Emmanueel <64825533+Emmanueel@users.noreply.github.com>
2020-07-20 10:24:09 -0500

    Add files via upload

---
+++ Ejercicio_de_limite.php
@@ -0,0 +1,12 @@
+<?php
+$min =1;
+$max =1;
+Print "<h2>El limite inferioe es: </h2><br>";
+for ($min=1; $min <50 ; $min++) { 
+    echo "$min";
+}
+Print "<h2> El limite superior es: </h2><br>";
+for ($max=1; $max <100 ; $max++) { 
+    echo "$max";
+}
+?>
